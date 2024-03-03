a682b74270ed8b7225c3a6f663ef79f425b6d3a0
Kiara Grouwstra <kiara@bij1.org>
2024-03-03 12:45:34 +0100

    update hash

--- themes/jsonresume-theme-stackoverflow-macchiato/default.nix
+++ themes/jsonresume-theme-stackoverflow-macchiato/default.nix
@@ -10,7 +10,8 @@ in
     inherit pname version;
     src = inputs.jsonresume-theme-stackoverflow-macchiato;
     dontNpmBuild = true;
-    npmDepsHash = "sha256-93GARhAJws1NBAjncFEcSwdJRrONVRuuMBDUDVsySdo=";
+    npmDepsHash = "sha256-MO0doEb7GsE+EGA/0rxE1+HwKKUelBmeHt5ouEN0XOs=";
+
     meta = {
       description = "Stack Overflow theme for JSON Resume";
       homepage = "https://github.com/phoinixi/jsonresume-theme-stackoverflow";
